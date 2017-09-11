648d84527d0576bb2d676f91b2a3c13b5894a146
aaron tolentino <aaron.tolentino@nuworks.ph>
2017-09-11 12:49:22 +0800

    add template module

---
+++ src/Modules/Template.php
@@ -0,0 +1,10 @@
+<?php
+namespace Deeptruth\Mailchimp\Modules;
+
+use Deeptruth\Mailchimp\Services\ModulesCrudService;
+
+class Template extends ModulesCrudService
+{
+	protected $module_name = 'templates';
+	
+}
