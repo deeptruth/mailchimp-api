4c8e174ae171fc2aa64a1736669690281274e702
aaron tolentino <aaron.tolentino@nuworks.ph>
2017-09-11 15:34:19 +0800

    fix bracket for psr 2

--- src/Modules/Member.php
+++ src/Modules/Member.php
@@ -8,7 +8,12 @@ class Member extends ModulesCrudService
 	protected $module_name = 'members';
 
 	/**
-	 * Set modulename with parent id
+	 * Set module_request_uri with list id and call construct
+	 *
+	 * @param String $api_key	API Key of the request
+	 * @param Array $args 		Parameter of class. This is array for flexibility in other and future modules.
+	 *
+	 * @return void
 	 */
 	public function __construct($api_key, $args)
 	{
@@ -17,13 +22,14 @@ class Member extends ModulesCrudService
 	}
 
 	/**
-	 * Overwrite setModuleRequestURI method with parent id
+	 * Overwrite setModuleRequestURI method with list id
 	 *
 	 * @param Array $args 		Parameter of class. This is array for flexibility in other and future modules.
 	 *
 	 * @return void
 	 */
-	public function setModuleRequestURI($args){
+	public function setModuleRequestURI($args)
+	{
 		$parent_id = $args[0];
 		$this->module_request_uri = "lists/$parent_id/members";
 	}
